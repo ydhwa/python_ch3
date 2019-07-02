9a0ecfc56630badeaa1d440e857421ceb834663e
ydhwa <ydhwa_18@naver.com>
2019-07-02 16:37:02 +0900

    wsgi 경로 설정 II

--- python_ch3/wsgi.py
+++ python_ch3/wsgi.py
@@ -1,20 +1,15 @@
 """
 WSGI config for python_ch3 project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
 import sys
-
-
-from django.core.wsgi import get_wsgi_application
+import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_ch3.settings')
-
 application = get_wsgi_application()
