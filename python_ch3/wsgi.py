3c530111c06fd8a9c0b27b3443d775370e7cca9c
ydhwa <ydhwa_18@naver.com>
2019-07-02 16:27:43 +0900

    wsgi 경로 설정

--- python_ch3/wsgi.py
+++ python_ch3/wsgi.py
@@ -10,9 +10,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 import sys
 
+
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_ch3.settings')
 
